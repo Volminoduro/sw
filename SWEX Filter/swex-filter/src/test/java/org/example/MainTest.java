fee929c10c8105fdedf41a61718b298cf05640f7
Volminoduro <steve.massicard@gmail.com>
2024-11-23 20:44:37 +0100

    Creating export file (WiP)

---
+++ SWEX Filter/swex-filter/src/test/java/org/example/MainTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class MainTest {
+
+    @Test
+    void main() {
+        fail();
+    }
+}
